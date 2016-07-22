29fdb528c8fcb9d2c06c08c97b07bc7a3e391b02
Debasis Panda <debcetb06@gmail.com>
2016-07-22 00:29:11 -0700

    middleware

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var app = express();
+var PORT = 3000;
 
-app.get('/', function( req, res ) {
-   res.send('Hello Debasis');
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
 
-
-
-app.listen(3000, function(){
-	console.log('Express Started!');
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
 });
