354b9b6d3ad3d2c58dd80ab2349bae06e0784e88
Debasis Panda <debcetb06@gmail.com>
2016-07-22 00:11:02 -0700

    Intialize Repo

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function( req, res ) {
+   res.send('Hello Debasis');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.listen(3000, function(){
+	console.log('Express Started');
+});
